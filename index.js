ddb3c2f2e5ce27fff8a9c6c9029ccf9e515d213e
Chester <chesterdeveloper2000@gmail.com>
2020-07-14 12:30:12 +0800

    tried to display simple html

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const app = express()
 
 
 app.get("/", (req, res) => {
-    res.send("hi there!")
+    res.send(`
+        <div>
+            <form>
+                <input type=email placeholder="Email" />
+                <input type=password placeholder="Password" />
+                <input type=password placeholder="Confirm Password" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `)
 })
 
 app.listen(3000, () => {
