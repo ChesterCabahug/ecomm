0bb5d72ecd15eb8acf450970b66b4f8d01408443
Chester <chesterdeveloper2000@gmail.com>
2020-07-14 14:55:30 +0800

    learned about post and get method (form submission types)

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require("express")
+const { restart } = require("nodemon")
 const app = express()
 
 
 app.get("/", (req, res) => {
     res.send(`
         <div>
-            <form>
-                <input type=email placeholder="Email" />
-                <input type=password placeholder="Password" />
-                <input type=password placeholder="Confirm Password" />
+            <form method="POST">
+                <input name="email" type=email placeholder="Email" />
+                <input name="password" type=password placeholder="Password" />
+                <input name="passwordConfirmation" type=password placeholder="Confirm Password" />
                 <button>Sign Up</button>
             </form>
         </div>
     `)
+})
+
+app.post("/", (req, res) => {
+    res.send("Account Created!")
 })
 
 app.listen(3000, () => {
