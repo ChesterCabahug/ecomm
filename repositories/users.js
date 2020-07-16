7d61154ebb2bc96acd598c6da2ed8f1cc0843b8f
Chester <chesterdeveloper2000@gmail.com>
2020-07-16 21:52:59 +0800

    learned about exporting a class more efficiently by adding new keyword

--- repositories/users.js
+++ repositories/users.js
@@ -77,16 +77,4 @@ class UsersRepository {
     }
 }
 
-
-const test = async () => {
-    const repo = new UsersRepository("users.json")
-
-    const user = await repo.getOneBy({ 
-        id: "e402c05f"
-    
-    })
-    console.log(user)
-}
-
-
-test()
+module.exports = new UsersRepository("users.json")
