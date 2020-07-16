0bde572bd8a7698d2658a05b8bb7da26a58c5d6d
Chester <chesterdeveloper2000@gmail.com>
2020-07-16 22:05:00 +0800

    created signup validation logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const { restart } = require("nodemon")
 const bodyParser = require("body-parser")
+const usersRepo = require("./repositories/users")
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -21,8 +22,18 @@ app.get("/", (req, res) => {
 
 
 
-app.post("/",(req, res) => {
-    console.log(req.body)
+app.post("/", async (req, res) => {
+    const {email, password, passwordConfirmation} = req.body
+
+    const existingUser = await usersRepo.getOneBy({email})
+
+    if(existingUser) {
+        return res.send("email in use")
+    }
+    if(password !== passwordConfirmation) {
+        return res.send("Passwords must match")
+    }
+    
     res.send("Account Created!")
 })
 
