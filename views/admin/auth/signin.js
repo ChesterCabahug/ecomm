f8ec0ba4b5cf76e63adbf361538615d964039c45
Chester <chesterdeveloper2000@gmail.com>
2020-07-17 16:03:34 +0800

    created layout.js to make sigin.js and signup.js have a complete html structure that's not error prone and not repeating

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,12 +1,15 @@
+const layout = require("../layout")
 module.exports = () => {
-    return `
-    <div>
-        <form method="POST">
-            <input name="email" type=email placeholder="Email" />
-            <input name="password" type=password placeholder="Password" />
-            
-            <button>Sign In</button>
-        </form>
-    </div>
-    `
+    return layout({
+        content: `
+            <div>
+                <form method="POST">
+                    <input name="email" type=email placeholder="Email" />
+                    <input name="password" type=password placeholder="Password" />
+                    
+                    <button>Sign In</button>
+                </form>
+            </div>
+            `
+    })
 }
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,16 @@
-module.exports = ( { req } ) => {
-    return `
-    <div>
-        Your id is: ${req.session.userId}
-        <form method="POST">
-            <input name="email" type=email placeholder="Email" />
-            <input name="password" type=password placeholder="Password" />
-            <input name="passwordConfirmation" type=password placeholder="Confirm Password" />
-            <button>Sign Up</button>
-        </form>
-    </div>
-    `
+const layout = require("../layout")
+module.exports = ({ req }) => {
+    return layout({
+        content: `
+            <div>
+                Your id is: ${req.session.userId}
+                <form method="POST">
+                    <input name="email" type=email placeholder="Email" />
+                    <input name="password" type=password placeholder="Password" />
+                    <input name="passwordConfirmation" type=password placeholder="Confirm Password" />
+                    <button>Sign Up</button>
+                </form>
+            </div>
+            `
+    })
 }
