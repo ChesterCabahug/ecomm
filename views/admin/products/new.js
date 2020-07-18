24cb95fb89bc9d5a65386789d511986049b2c74e
Chester <chesterdeveloper2000@gmail.com>
2020-07-18 20:32:34 +0800

    -created some validators for the admin create page (requireTitle & requriePrice) -used the validator in routes/admin/products.js and console logged it if it works -fixed from views/admin/producs/new.js the form tag.. from action(whutt??) to method

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -8,7 +8,7 @@ module.exports = ({
 }) => {
     return layout({
         content: `
-            <form action="POST">
+            <form method="POST">
                 <input type="text" placeholder="Title" name="title">
                 <input type="text" placeholder="Price" name="price">
                 <input type="file" name="image">
