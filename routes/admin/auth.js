6ab58f96765d45efb89e1d38c333539b20499f1e
Chester <chesterdeveloper2000@gmail.com>
2020-07-18 11:23:44 +0800

    extracted validation chains into a new file called validators.js.. then added it to a new file

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -3,6 +3,7 @@ const { check, validationResult } = require("express-validator")
 const usersRepo = require("../../repositories/users")
 const signupTemplate = require("../../views/admin/auth/signup")
 const signinTemplate = require("../../views/admin/auth/signin")
+const { requireEmail, requirePassword, requirePasswordConfirmation } = require("./validators")
 
 const router = express.Router()
 
@@ -12,28 +13,12 @@ router.get("/signup", (req, res) => {
 
 
 
-router.post("/signup", [
-    check("email").trim().normalizeEmail().isEmail(),
-    check("password").trim().isLength({ min: 4, max: 20 }),
-    check("passwordConfirmation").trim().isLength({min: 4, max:20})
-], async (req, res) => {
+router.post("/signup", [requireEmail, requirePassword, requirePasswordConfirmation], 
+    async (req, res) => {
     const errors = validationResult(req)
     console.log(errors)
     const {email, password, passwordConfirmation} = req.body
-
-    const existingUser = await usersRepo.getOneBy({email})
-
-    if(existingUser) {
-        return res.send("email in use")
-    }
-    if(password !== passwordConfirmation) {
-        return res.send("Passwords must match")
-    }
-    
-    // create a user in our user repo to represent this person
     const user = await usersRepo.create({email, password})
-
-    // store the id of that user inside the users' cookie
     req.session.userId = user.id
 
 
