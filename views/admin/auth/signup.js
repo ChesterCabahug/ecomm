08fcc02311b7d7609220845a58547ed098e8fcfc
Chester <chesterdeveloper2000@gmail.com>
2020-07-18 12:07:07 +0800

    allowed errors to display next to the input field if user types an invalid email, a pw that isn't correct

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,26 @@
 const layout = require("../layout")
-module.exports = ({ req }) => {
+
+const getError = (errors, prop) => {
+    // prop === "email" || "password" || "passwordConfirmation"
+    try {
+        return errors. mapped()[prop].msg
+    } catch (err) {
+        return ""
+    }
+}
+
+module.exports = ({ req, errors }) => {
     return layout({
         content: `
             <div>
                 Your id is: ${req.session.userId}
                 <form method="POST">
-                    <input name="email" type=email placeholder="Email" />
-                    <input name="password" type=password placeholder="Password" />
-                    <input name="passwordConfirmation" type=password placeholder="Confirm Password" />
+                    <input name="email" type=name placeholder="Email" />
+                    ${getError(errors, "email")}
+                    <input name="password" type=name placeholder="Password" />
+                    ${getError(errors, "password")}
+                    <input name="passwordConfirmation" type=name placeholder="Confirm Password" />
+                    ${getError(errors, "passwordConfirmation")}
                     <button>Sign Up</button>
                 </form>
             </div>
