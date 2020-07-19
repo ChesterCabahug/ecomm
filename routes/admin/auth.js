4ee38d78e959f3d0de5b5963b8307ed7d8b78709
Chester <chesterdeveloper2000@gmail.com>
2020-07-19 16:08:36 +0800

    refactored stuffs that have the same logic and put it in routes/admin/middlewares.js

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,8 +1,8 @@
 const express = require("express")
+
 const {
-    check,
-    validationResult
-} = require("express-validator")
+    handleErrors
+} = require("./middlewares")
 const usersRepo = require("../../repositories/users")
 const signupTemplate = require("../../views/admin/auth/signup")
 const signinTemplate = require("../../views/admin/auth/signin")
@@ -24,19 +24,11 @@ router.get("/signup", (req, res) => {
 
 
 
-router.post("/signup", [requireEmail, requirePassword, requirePasswordConfirmation],
+router.post("/signup", [requireEmail, requirePassword, requirePasswordConfirmation], handleErrors(signupTemplate),
     async (req, res) => {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            return res.send(signupTemplate({
-                req,
-                errors
-            }))
-        }
         const {
             email,
             password,
-            passwordConfirmation
         } = req.body
         const user = await usersRepo.create({
             email,
@@ -61,14 +53,8 @@ router.post("/signin", [
         requireEmailExists,
         requireValidPasswordForUser
     ],
+    handleErrors(signinTemplate),
     async (req, res) => {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            res.send(signinTemplate({
-                errors
-            }))
-        }
-
         const {
             email
         } = req.body
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,7 @@
 const express = require("express")
-const {
-    validationResult
-} = require("express-validator")
 const multer = require("multer")
 
-
+const {handleErrors} = require("./middlewares")
 const productsRepo = require("../../repositories/products")
 const productsNewTemplate = require("../../views/admin/products/new")
 const {
@@ -27,12 +24,7 @@ router.get("/admin/products/new", (req, res) => {
     res.send(productsNewTemplate({}))
 })
 
-router.post("/admin/products/new", upload.single("image"), [requireTitle, requirePrice], async (req, res) => {
-    const errors = validationResult(req)
-
-    if(!errors.isEmpty()){
-        return res.send(productsNewTemplate({ errors }))
-    }
+router.post("/admin/products/new", upload.single("image"), [requireTitle, requirePrice], handleErrors(productsNewTemplate), async (req, res) => {
 
     const image = req.file.buffer.toString("base64")
     const {
