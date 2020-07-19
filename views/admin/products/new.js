c23e0489f18a8fc0ff466008fb39b7b3875d4b12
Chester <chesterdeveloper2000@gmail.com>
2020-07-19 13:56:16 +0800

    -middleware bug spotted when if(!errors.isEmpty()) was added in products.js -bug is that even when we put the right values in the input fields, the app says that it's still wrong -realized that this was happening because [requireTitle, requirePrice] isn't working anymore -solution: swaped places.. upload.single('image') and [requireTitle, requirePrice]

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -2,6 +2,7 @@ const layout = require("../layout")
 const {
     getError
 } = require("../../helpers")
+const { get } = require("../../../routes/admin/auth")
 
 module.exports = ({
     errors
@@ -10,7 +11,9 @@ module.exports = ({
         content: `
             <form method="POST" enctype="multipart/form-data">
                 <input type="text" placeholder="Title" name="title">
+                ${getError(errors, "title")}
                 <input type="text" placeholder="Price" name="price">
+                ${getError(errors, "price")}
                 <input type="file" name="image">
                 <button>Submit</button>
             </form>
