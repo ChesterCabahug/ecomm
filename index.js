a84887aba51b6d06f0c388dc185d3c48c81bcdc3
Chester <chesterdeveloper2000@gmail.com>
2020-07-20 15:00:46 +0800

    -CART FEATURE STARTED! -created a new products route file then linked it to main index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cookieSession = require("cookie-session")
 const authRouter = require("./routes/admin/auth")
 const adminProductsRouter = require("./routes/admin/products")
 const productsRouter = require("./routes/products")
+const cartsRouter = require("./routes/carts")
 
 const app = express()
 
@@ -21,6 +22,7 @@ app.use(cookieSession({
 app.use(authRouter)
 app.use(adminProductsRouter)
 app.use(productsRouter)
+app.use(cartsRouter)
 
 
 
---
+++ routes/carts.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const router = express.Router()
+
+// receive post request to add an item to the cart
+
+// receive a get request to show all items to cart
+
+// receive a post to delete from cart
+
+module.exports = router
