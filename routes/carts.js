b4d545d1f68d2e5376e5988514a7665a3ca15788
Chester <chesterdeveloper2000@gmail.com>
2020-07-20 18:36:18 +0800

    -creating add to cart functionality if the user actually clicks on it... it creates a cookie

--- routes/carts.js
+++ routes/carts.js
@@ -1,10 +1,30 @@
 const express = require("express")
+const cartsRepo = require("../repositories/carts")
 
 const router = express.Router()
 
 // receive post request to add an item to the cart
-router.post("/cart/products", (req, res) => {
-    console.log(req.body.productId)
+router.post("/cart/products", async (req, res) => {
+    // figure out the cart
+    let cart
+    if(!req.session.cartId) {
+        // we don't have a cart, we need to create one,
+        // and store the cart id on the req.session.cartId
+        // property
+
+        cart = await cartsRepo.create({items: []})
+
+        req.session.cartId = cart.id
+    } else {
+        // we have a cart, let's get it from the repository
+        cart = await cartsRepo.getOne(req.session.cartId)
+    }
+
+    console.log(cart)
+    // either increment quantity of existing product or 
+    // OR add new product items array
+
+
     res.send("product added to cart!")
 })
 
