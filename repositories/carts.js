a2a2a5e46d104a94db86bd75d733421bf5379161
Chester <chesterdeveloper2000@gmail.com>
2020-07-20 14:53:40 +0800

    -CART FEATURE STARTED! -creating a carts repository class

---
+++ repositories/carts.js
@@ -0,0 +1,8 @@
+const repository = require("./repository");
+const Repository = require("./repository");
+
+class CartsRepository extends Repository {
+
+}
+
+module.exports = new CartsRepository("carts.json")
