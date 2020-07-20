ceb944d61d479ec715b931d73bf560027ab4c07e
Chester <chesterdeveloper2000@gmail.com>
2020-07-20 11:34:54 +0800

    fixed that bug that if you leave the field empty it throws a nasty error..

--- views/admin/products/edit.js
+++ views/admin/products/edit.js
@@ -1,13 +1,19 @@
+const {
+    getError
+} = require("../../helpers")
 const layout = require("../layout")
 
 module.exports = ({
-    product
+    product,
+    errors
 }) => {
     return layout({
         content: `
             <form method="POST">
                 <input name="title" value="${product.title}" />
+                ${getError(errors, "title")}
                 <input name="price" value="${product.price}" />
+                ${getError(errors, "price")}
                 <input name="image" type="file" />
                 <button>Submit</button>
             </form>
