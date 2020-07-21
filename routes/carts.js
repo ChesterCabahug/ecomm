465c6715647baf7a1001e31a0df266aba700fe6f
Chester <chesterdeveloper2000@gmail.com>
2020-07-21 10:31:57 +0800

    cart page created!!! it now works when you click on the carts page from the front page.. (if you added something from cart)

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const cartsRepo = require("../repositories/carts")
+const productsRepo = require("../repositories/products")
+const cartShowTemplate = require("../views/carts/show")
 
 const router = express.Router()
 
@@ -36,6 +38,21 @@ router.post("/cart/products", async (req, res) => {
 })
 
 // receive a get request to show all items to cart
+router.get("/cart",async (req, res) => {
+    if(!req.session.cartId) {
+        return res.redirect("/")
+    }
+
+    const cart = await cartsRepo.getOne(req.session.cartId)
+
+    for (let item of cart.items) {
+        const product = await productsRepo.getOne(item.id)
+
+        item.product = product
+    }
+
+    res.send(cartShowTemplate({items: cart.items}))
+})
 
 // receive a post to delete from cart
 
