8e91da663285c2845b1aa237f135ac4b96bdfb1e
Chester <chesterdeveloper2000@gmail.com>
2020-09-04 20:48:30 +0800

    refactor

--- repositories/users.js
+++ repositories/users.js
@@ -1,40 +1,37 @@
-const fs = require("fs")
-const crypto = require("crypto")
-const util = require("util")
-const Repository = require("./repository")
+const fs = require("fs");
+const crypto = require("crypto");
+const util = require("util");
+const Repository = require("./repository");
 
-const scrypt = util.promisify(crypto.scrypt)
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository extends Repository {
-
     async comparePasswords(saved, supplied) {
         // saved -> password saved in our database. "hashed.salt"
         // supplied -> password given by a user trying to sign in
 
-        const [hashed, salt] = saved.split(".")
-        const hashedSuppliedBuf = await scrypt(supplied, salt, 64)
+        const [hashed, salt] = saved.split(".");
+        const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
 
-        return hashed === hashedSuppliedBuf.toString("hex")
+        return hashed === hashedSuppliedBuf.toString("hex");
     }
 
     async create(attrs) {
-        attrs.id = this.randomID()
-        const salt = crypto.randomBytes(8).toString("hex")
-        const buf = await scrypt(attrs.password, salt, 64)
+        attrs.id = this.randomID();
+        const salt = crypto.randomBytes(8).toString("hex");
+        const buf = await scrypt(attrs.password, salt, 64);
 
-        const records = await this.getAll()
+        const records = await this.getAll();
         const record = {
             ...attrs,
-            password: `${buf.toString("hex")}.${salt}`
-        }
-        records.push(record)
+            password: `${buf.toString("hex")}.${salt}`,
+        };
+        records.push(record);
 
-        await this.writeAll(records)
+        await this.writeAll(records);
 
-        return record
+        return record;
     }
-
-    
 }
 
-module.exports = new UsersRepository("users.json")
+module.exports = new UsersRepository("users.json");
