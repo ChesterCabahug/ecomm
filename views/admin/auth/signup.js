191a88967d9402fcf2514a0caaf415c8ed498719
Chester <chesterdeveloper2000@gmail.com>
2020-09-05 20:39:58 +0800

    refactor

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,10 +1,7 @@
-const layout = require("../layout")
-const { getError } = require("../../helpers")
+const layout = require("../layout");
+const { getError } = require("../../helpers");
 
-module.exports = ({
-    req,
-    errors
-}) => {
+module.exports = ({ req, errors }) => {
     return layout({
         content: `
                 <div class="container">
@@ -15,19 +12,25 @@ module.exports = ({
                     <div class="field">
                         <label class="label">Email</label>
                         <input required class="input" placeholder="Email" name="email" />
-                        <p class="help is-danger">${getError(errors, 'email')}</p>
+                        <p class="help is-danger">${getError(
+                            errors,
+                            "email"
+                        )}</p>
                     </div>
                     <div class="field">
                         <label class="label">Password</label>
                         <input required class="input" placeholder="Password" name="password" type="password" />
-                        <p class="help is-danger">${getError(errors, 'password')}</p>
+                        <p class="help is-danger">${getError(
+                            errors,
+                            "password"
+                        )}</p>
                     </div>
                     <div class="field">
                         <label class="label">Password Confirmation</label>
                         <input required class="input" placeholder="Password Confirmation" name="passwordConfirmation" type="password" />
                         <p class="help is-danger">${getError(
-                        errors,
-                        'passwordConfirmation'
+                            errors,
+                            "passwordConfirmation"
                         )}</p>
                     </div>
                     <button class="button is-primary">Submit</button>
@@ -36,6 +39,6 @@ module.exports = ({
                 </div>
                 </div>
             </div>
-            `
-    })
-}
+            `,
+    });
+};
